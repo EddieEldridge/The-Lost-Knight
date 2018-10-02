79954afe5fc4cc8803c02a0afbc65eef02f902fe
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-02 09:42:03 +0000

    Added basic touch controls

--- TheLostKnightUnity/Assets/Scripts/PlayerMovement.cs
+++ TheLostKnightUnity/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     // Variables
-    public float maxSpeed = 3.5f;
+    public float moveSpeed = 3.5f;
+    public bool moveRight;
+    public bool moveLeft;
+    public Rigidbody2D rigidbody;
     //float shipBoundaryRadius = 5f;
 
     void Start ()
     {
-		
+		rigidbody=GetComponent<Rigidbody2D>();
 	}
 
     void Update()
@@ -20,12 +23,23 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 pos = transform.position;
 
         // Movement for the ship (Vertical)
-        pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
-        transform.position = pos;
+        //pos.y += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+       // transform.position = pos;
 
         // Movement for the ship (Horizontal)
-        pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
-        transform.position = pos;
+       // pos.x += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+       // transform.position = pos;
+
+
+        // Android touch movement
+        if (moveRight)
+        {
+            rigidbody.velocity = new Vector2(moveSpeed, rigidbody.velocity.y);
+        }
+        if (moveLeft)
+        {
+            rigidbody.velocity = new Vector2(-moveSpeed, rigidbody.velocity.y);
+        }
 
         // Restricts the player to camera view
         pos = Camera.main.WorldToViewportPoint(transform.position);
---
+++ TheLostKnightUnity/Assets/Scripts/TouchMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchMovement : MonoBehaviour
+        {
+            private PlayerMovement player;
+
+
+            void Start()
+            {
+                player = FindObjectOfType<PlayerMovement>();
+            }
+
+            public void LeftArrow()
+            {
+                player.moveRight = false;
+                player.moveLeft = true;
+            }
+            public void RightArrow()
+            {
+                player.moveRight = true;
+                player.moveLeft = false;
+            }
+            public void ReleaseLeftArrow()
+            {
+                player.moveLeft = false;
+            }
+            public void ReleaseRightArrow()
+            {
+                player.moveRight = false;
+
+            }
+
+        }
