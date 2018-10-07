ab1854007507951a2139f51f584db6dadcfa2828
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-07 12:38:46 +0000

    Added camera follow script

---
+++ TheLostKnightUnity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 15;
+
+    // Function to move our camera with our target 
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y+3, transform.position.z), Time.deltaTime*smoothSpeed);
+    }
+}
