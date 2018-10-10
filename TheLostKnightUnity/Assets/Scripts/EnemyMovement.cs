878a94da6d23ba9200a9e3665facec8f928981f5
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-10 13:01:46 +0000

    Refactored enemy movement so the enemy will now move between two points

--- TheLostKnightUnity/Assets/Scripts/EnemyMovement.cs
+++ TheLostKnightUnity/Assets/Scripts/EnemyMovement.cs
@@ -2,13 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
-
-    public float fallSpeed = 8.0f;
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
-
+//Moves object between two points
+public class EnemyMovement : MonoBehaviour
+ {
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    [SerializeField] private bool reverseMove = false;
+    [SerializeField] private Transform objectToUse;
+    [SerializeField] private bool moveThisObject = false;
+    private float startTime;
+    private float journeyLength;
+    private float distCovered;
+    private float fracJourney;
+    
+    void Start()
+    {
+        startTime = Time.time;
+        if (moveThisObject)
+        {
+            objectToUse = transform;
+        }
+        journeyLength = Vector3.Distance(pointA.transform.position, pointB.transform.position);
+    }
+    void Update()
+    {
+        distCovered = (Time.time - startTime) * moveSpeed;
+        fracJourney = distCovered / journeyLength;
+        if (reverseMove)
+        {
+            objectToUse.position = Vector3.Lerp(pointB.transform.position, pointA.transform.position, fracJourney);
+        }
+        else
+        {
+            objectToUse.position = Vector3.Lerp(pointA.transform.position, pointB.transform.position, fracJourney);
+        }
+        if ((Vector3.Distance(objectToUse.position, pointB.transform.position) == 0.0f || Vector3.Distance(objectToUse.position, pointA.transform.position) == 0.0f)) //Checks if the object has travelled to one of the points
+        {
+            if (reverseMove)
+            {
+                reverseMove = false;
+            }
+            else
+            {
+                reverseMove = true;
+            }
+            startTime = Time.time;
+        }
     }
 }
