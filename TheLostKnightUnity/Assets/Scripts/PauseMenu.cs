2686dc90141a22c488ead6e9efbd453814b14e6f
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-22 14:30:19 +0000

    Added a pause button

---
+++ TheLostKnightUnity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Variables
+	public bool GameIsPaused = false;
+	public GameObject PauseMenuUi;
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(GameIsPaused == true)
+		{
+			Pause();
+		}
+		else
+		{
+			Resume();
+		}
+	}
+
+	void Resume()
+	{
+		PauseMenuUi.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	void Pause()
+	{
+		PauseMenuUi.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+}
--- TheLostKnightUnity/Assets/Scripts/TouchMovement.cs
+++ TheLostKnightUnity/Assets/Scripts/TouchMovement.cs
@@ -6,11 +6,13 @@ public class TouchMovement : MonoBehaviour
     // Variables
     private PlayerMovement player;
     private PlayerAttack playerAttacking;
+    private PauseMenu pauseMenu;
 
      void Start()
     {
                 player = FindObjectOfType<PlayerMovement>();
                 playerAttacking = FindObjectOfType<PlayerAttack>();
+                pauseMenu = FindObjectOfType<PauseMenu>();
     }
 
     public void LeftArrow()
@@ -63,6 +65,18 @@ public class TouchMovement : MonoBehaviour
     public void ReleaseAttackButton()
     {
         playerAttacking.isAttacking=false;
+    }
+
+    public void PauseButton()
+    {
+        if(pauseMenu.GameIsPaused == true)
+        {
+            pauseMenu.GameIsPaused = false;
+        }
+        else if(pauseMenu.GameIsPaused == false)
+        {
+            pauseMenu.GameIsPaused = true;
+        }
     }
 
 }
