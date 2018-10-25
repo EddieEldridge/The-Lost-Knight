c160d97ce3a2b8644de68ef6e4f345787a2d0e05
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-25 13:51:43 +0000

    Added sound effect on coin pickup

--- TheLostKnightUnity/Assets/Scripts/ItemCount.cs
+++ TheLostKnightUnity/Assets/Scripts/ItemCount.cs
@@ -7,18 +7,24 @@ public class ItemCount : MonoBehaviour
 {
 
     private TextDisplay textDisplay;
+    public AudioSource coinFX;
+    AudioClip coinFXClip;
 
     void Start()
     {
         textDisplay = FindObjectOfType<TextDisplay>();
+        GetComponent<AudioSource>().playOnAwake = false;
+        coinFX=GetComponent<AudioSource>();
+        coinFXClip = coinFX.clip;
     }
 
     void OnTriggerEnter2D(Collider2D collidingWith)
     {
         if (collidingWith.CompareTag("Player"))
         {
+            coinFX.Play();        
             textDisplay.coinAmount++;
-            Destroy(gameObject);
+            Destroy(gameObject, coinFXClip.length);
         }
     }
 }
