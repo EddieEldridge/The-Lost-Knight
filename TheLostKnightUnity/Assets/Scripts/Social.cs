1f76d5c69c949064d62878e543b3cac017aa163e
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-11-22 12:37:45 +0000

    Addded Facebook Social integration

--- TheLostKnightUnity/Assets/Scripts/Social.cs
+++ TheLostKnightUnity/Assets/Scripts/Social.cs
@@ -1,16 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
 
-public class Social : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Social : MonoBehaviour
+{
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	  private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Couldn't initialize");
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+ 
+    #region Login / Logout
+    public void FacebookLogin()
+    {
+        var permissions = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(permissions);
+    }
+ 
+    public void FacebookLogout()
+    {
+        FB.LogOut();
+    }
+    #endregion
+ 
+    public void FacebookShare()
+    {
+        FB.ShareLink(new System.Uri("https://resocoder.com"), "Check it out!",
+            "Good programming tutorials lol!",
+            new System.Uri("https://resocoder.com/wp-content/uploads/2017/01/logoRound512.png"));
+    }
+ 
+    #region Inviting
+    public void FacebookGameRequest()
+    {
+        FB.AppRequest("Hey! Come and play this awesome game!", title: "The Lost Knight");
+    }
+ 
+    public void FacebookInvite()
+    {
+        FB.Mobile.AppInvite(new System.Uri("https://play.google.com/store/apps/details?id=com.tappybyte.byteaway"));
+    }
+    #endregion
+ 
+  
+
+
 }
