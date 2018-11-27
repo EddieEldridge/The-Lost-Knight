0474f0ba262914c0d954fb64e2a32132cfe562a3
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-11-27 17:07:47 +0000

    Fixed death screen bug

--- TheLostKnightUnity/Assets/Scripts/PlayerHealth.cs
+++ TheLostKnightUnity/Assets/Scripts/PlayerHealth.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerHealth : MonoBehaviour
@@ -15,14 +16,24 @@ public class PlayerHealth : MonoBehaviour
     public float playerHealth;
     float healthPercentage;
     public GameObject UI;
+    public GameObject deathText;
     private SplashFade splashFade;
+    Scene currentScene;
+    string sceneName;
+
+
 
     private CollisionDamage collisionDamage;
 
+    void Awake()
+    {
+        deathText.SetActive(false);
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+}
     // Use this for initialization
     void Start()
     {
-		
         GameObject healthBarObject = GameObject.FindGameObjectWithTag("HealthBar");
 
         splashFade = FindObjectOfType<SplashFade>();
@@ -43,8 +54,10 @@ public class PlayerHealth : MonoBehaviour
         {
             if (UI != null)
             {
-                splashFade.isDead=true;
+                splashFade.isDead = true;
                 UI.SetActive(false);
+                deathText.SetActive(true);
+                SceneManager.LoadScene(sceneName);
             }
         }
     }
